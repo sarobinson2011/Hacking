be58defd901484971bb44b97781e251ad5ae20e8
sarobinson2001 <sarobinson2011@gmail.com>
2022-09-08 12:54:15 +0100

    first commit

--- Crack-PDF-password.py
+++ Crack-PDF-password.py
@@ -17,7 +17,7 @@ for line in open("/home/oem/Documents/wordlist.txt"):
 for password in tqdm(passwords, "Attempting to brute force attack PDF password"):
     try:
 
-        # open PDF file and check each password
+        # open PDF file and check each password in turn
         with pikepdf.open("/home/oem/Documents/Quotes.pdf", password=password) as p:
             # If password is correct, break the loop
             print("[+] Password found:", password)
