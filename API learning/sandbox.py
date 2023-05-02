c1395c8c2f0adea6da7c23619c8261a17d3e491e
sarobinson2001 <sarobinson2011@gmail.com>
2023-05-02 08:56:00 +0100

    added sandbox file

---
+++ API learning/sandbox.py
@@ -0,0 +1,17 @@
+alien_0 = {'color': 'green',
+           'points': 5,
+           'name': 'Beast',
+           'age': 10}
+alien_1 = {'color': 'yellow',
+           'points': 15,
+           'name': 'Marvin',
+           'age': 3}
+alien_2 = {'color': 'red',
+           'points': 8,
+           'name': 'Toby',
+           'age': 14}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(f"\n", alien)
